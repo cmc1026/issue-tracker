cecf4a64badd3e4c73106071227e09128a8b6da0
cmc1026 <127949422+cmc1026@users.noreply.github.com>
2024-04-04 13:13:52 -0400

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,8 +4,8 @@ import prisma from '@prisma/client';
 import { PrismaClient } from '@prisma/client'
 
 const issueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Description is required')
 })
 
 export async function POST(request: NextRequest){
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest){
     const validation = issueSchema.safeParse(body);
 
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.format(), {status: 400})
 
     const prisma = new PrismaClient();
     
